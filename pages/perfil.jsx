82d97e21cf75f7497ccd2e8dc78d2f92170d9bda
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2023-05-29 16:46:56 -0300

    deploy vercel

--- pages/perfil.jsx
+++ pages/perfil.jsx
@@ -22,10 +22,10 @@ const perfil = () => {
         
                  
                   
-                        <div class=" flex flex-cols-2 glass rounded-xl m-auto mb-96  bg-transparent-100 shadow-lg w-5/12 ">
+                        <div class=" flex flex-cols-2 glass rounded-xl m-auto mb-96  bg-transparent-100 shadow-lg w-7/12 ">
                             <img class="mb-20 max-w-auto m-10 w-80 mr-20 h-64 rounded-lg " src={usuario.foto} alt="" />
                             <div class="  text-center  p-4 w-98 rounded-lg  ">
-                                <div className='mr-80 p-4 border border-sky-900 shadow-lg rounded-lg'>
+                                <div className='mr-80 p-4 '>
                                     <div className='flex text-3xl  text-sky-900 font-medium mt-10 mb-20'>
                                 <p >
                                         Bienvenido </p>
